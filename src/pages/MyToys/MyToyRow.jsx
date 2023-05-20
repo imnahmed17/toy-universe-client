761c29d9ce58540cc8f9104534aea29f3c4b4ba2
helloashik17 <asiknoman150@gmail.com>
2023-05-21 05:29:40 +0600

    toy deleted added

--- src/pages/MyToys/MyToyRow.jsx
+++ src/pages/MyToys/MyToyRow.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 
 const MyToyRow = ({ toy, handleDelete }) => {
     const { _id, photo, toyName, subCategory, price, quantity, description } = toy;
@@ -10,11 +10,18 @@ const MyToyRow = ({ toy, handleDelete }) => {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                 </button>
             </th>
+            <td>
+                <div className="avatar">
+                    <div className="rounded w-16 h-[117px]">
+                        {photo && <img src={photo} alt="" />}
+                    </div>
+                </div>
+            </td>
             <td>{toyName.substring(19, 60)}</td>
             <td className="text-center">{subCategory}</td>
             <td>${price}</td>
             <td className="text-center">{quantity}</td>
-            <td>{description.substring(0, 50)}...</td>
+            <td>{description.substring(0, 40)}...</td>
             <td>
                 <button className="btn btn-sm btn-active btn-ghost">Update</button>
             </td>
--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
+import Swal from "sweetalert2";
 import MyToyRow from './MyToyRow';
 
 const MyToys = () => {
@@ -13,7 +14,47 @@ const MyToys = () => {
             .then((data) => setToys(data));
     }, [url]);
 
-    const handleDelete = (id) => {};
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#F87272",
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel!",
+        }).then(result => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/toys/${id}`, {
+                    method: "DELETE"
+                })
+                    .then((res) => res.json())
+                    .then((data) => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your toy has been deleted.",
+                                icon: "success",
+                                showConfirmButton: false,
+                                timer: 900,
+                            });
+                            const remaining = toys.filter(toy => toy._id !== id);
+                            setToys(remaining);
+                        }
+                    });
+            } else if (result.dismiss === Swal.DismissReason.cancel) {
+                Swal.fire({
+                    title: "Cancelled!",
+                    text: "Your toy is safe :)",
+                    icon: "error",
+                    showConfirmButton: false,
+                    timer: 900,
+                });
+            }
+        });
+    };
 
     return (
         <div className="mt-10">
@@ -23,6 +64,7 @@ const MyToys = () => {
                     <thead>
                         <tr>
                             <th></th>
+                            <th className="text-center">Image</th>
                             <th>Toy Name</th>
                             <th>Sub Category</th>
                             <th>Price</th>
